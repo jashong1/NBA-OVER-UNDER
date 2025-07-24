13ba52ae9d1539d2324db4d22d54d4f9fadf317f
jashong1 <jashong1@students.kennesaw.edu>
2025-07-23 22:20:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # WNBA Over/Under Prediction Model
-# Edited from NBA to WNBA since WNBA in currently in season and NBA is not. Will be able to test if model can accurate obtain profits long/short-term
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import csv
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, r2_score
 
 # Find a way to download Datasets!!!!!
-# Use basketball-reference for datasets
-data = pd.read_csv('wnba_totals_sample.csv')
+data = pd.read_csv('C:/Users/Home/Downloads/sportsref_download.csv')
 
 # Preview data
 data.head()
@@ -20,7 +20,7 @@ data.head()
 features = ['pace', 'off_eff', 'def_eff', 'home', 'rest_days', 'injuries']
 X = data[features]
 y = data['actual_total']
-
+    
 # Split the data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
